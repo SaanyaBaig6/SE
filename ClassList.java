216a641d34b4726ef476f25852a885a2cc74696c
saanyabaig6 <saanyabaig6@gmail.com>
2020-08-30 18:43:29 -0700

    initcommit

---
+++ ClassList.java
@@ -0,0 +1,46 @@
+package classlist;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ClassList {
+	Map<String,String> classList = new HashMap<String,String>();
+	
+	public ClassList() {
+		classList.put("Bob", "A");
+		classList.put("Mary", "C");
+		classList.put("Sarah", "B");
+		classList.put("Philip", "A");
+		classList.put("Greg", "F");
+	}
+	
+	public void printClassList() {
+		Iterator<Entry<String,String>> entries = classList.entrySet().iterator();
+		while (entries.hasNext()) {
+			Entry<String,String> entry = entries.next();
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+	}
+	
+	public void printClassList2() {
+		for (Map.Entry<String, String> entry : classList.entrySet()) {
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
+		
+		for (String key : classList.keySet()) {
+			System.out.println("Key = " + key);
+		}
+		
+		for (String value : classList.values()) {
+			System.out.println("Value = " + value);
+		}
+	}
+	
+	public static void main(String[] args) {
+		ClassList myClassList = new ClassList();
+		myClassList.printClassList();
+	}
+
+}
